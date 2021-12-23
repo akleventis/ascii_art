930377105240f1111db5db31dd6cafc0b62f3fd9
Ubuntu <ubuntu@ip-172-31-91-32.ec2.internal>
2021-12-23 18:17:09 +0000

    add switch

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    var options = {root: path.join(__dirname)};
-    var file = 'art.txt';
+app.get('/:key', (req, res) => {
+    var options = {root: path.join(__dirname, '/ascii_art')};
+    const file;
+    switch (req.params.key){
+      case 'swag':
+        file = 'swag.txt'
+        break;
+      case 'christmas':
+        file = 'christmas.txt'
+        break;
+      default:
+        file = 'bitly.txt'
+    }
     res.sendFile(file, options)
 })
 
