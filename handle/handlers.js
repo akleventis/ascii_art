5a1929e81dd1bde54508be14f6838b29ec10b5c8
Alex Leventis <alex.leventis1@gmail.com>
2021-12-27 18:47:46 -0800

    add ascii

--- handle/handlers.js
+++ handle/handlers.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 moment().format();
 const { files,  holidays } = require('./constants.js')
 
-
+// /holiday
 const getHoliday = (dateTest) => {
     const now = dateTest ? moment(new Date(dateTest)) : moment();
 
@@ -18,12 +18,14 @@ const getHoliday = (dateTest) => {
     return holidays[key][2]; 
 }
 
+// /random
 const getRandomFile = () => {
     const items = [files.swag, files.fire, files.adventuretime, files.cats, files.dogs, files.southpark, files.starwars, files.shred, files.yolo, files.flowers];
     const item = items[Math.floor(Math.random()*items.length)];
     return item;
 }
 
+// /:keyword
 const getAscii = (keyword) => {
     return (files[keyword] ? files[keyword] : files.bitly)
 }
